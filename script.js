02691c6e8707e6cac549e855a5cb87c633757e3c
Athul V <65706338+asuru56@users.noreply.github.com>
2024-01-14 18:41:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const bodyE = document.querySelector('body');
+for(let i = 0 ; i < 16; i++){
+const divElement = document.createElement("div"); 
+
+bodyE.appendChild(divElement);
+bodyE.style.backgroundColor = 'black';
+
+}
+
