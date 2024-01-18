d47f8683aa8b6ff1b767024c7cbedc67a6b9698f
Athul V <65706338+asuru56@users.noreply.github.com>
2024-01-18 19:05:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-const bodyE = document.querySelector('body');
-for(let i = 0 ; i < 16; i++){
-const divElement = document.createElement("div"); 
+const body = document.querySelector('body');
+const gridContainer = body.querySelector('.gridContainer');
 
-bodyE.appendChild(divElement);
-divElement.style.backgroundColor = 'black';
 
-}
+const resetButton = document.getElementById('resetButton');
 
+resetButton.addEventListener('click', function () {
+    let k = prompt('Enter the number of squares per side (maximum 100):');
+    if (k < 100) {
+        for (let i = 0; i < k; i++) {
+            // Create and append grid elements to the .bottom container
+            const bottomElement = document.createElement('div');
+            bottomElement.className = 'bottom';
+
+            for (let j = 0; j < k; j++) {
+                const grid = document.createElement('div');
+                grid.className = 'grid';
+                bottomElement.appendChild(grid);
+
+                // Attach a hover event listener to each grid element
+                grid.addEventListener('mouseenter', function () {
+                    // Change the color of the hovered grid element
+                    this.style.backgroundColor = 'black';
+                });
+            }
+
+            gridContainer.appendChild(bottomElement);
+        }
+    }
+
+});
+
+// Create and append grid elements to the .top container
---
+++ script2.js
@@ -0,0 +1,5 @@
+let vari = [10,20,30,40]; 
+var twice = vari.map((item,index,array)=>{
+    return item*2;
+})
+console.log(twice); 
